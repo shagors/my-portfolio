38a6e1505cf84a0e97018f9e4bef726347834cee
shagors <shajjadshagor@gmail.com>
2024-03-16 13:21:23 +0600

    some components make and design and make some changes for design and check it

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,11 +13,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} max-w-7xl mx-auto`}>
+        <ThemeProvider attribute="class" defaultTheme="light">
+          <Header />
+          {children}
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,29 @@
-const Header = () => {
-  return <header>Header</header>;
-};
+"use client";
 
-export default Header;
+import Logo from "./Logo";
+import MobileNav from "./MobileNav";
+import Nav from "./Nav";
+import ThemeToggler from "./ThemeToggler";
+import { useState } from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <Logo />
+          <div className="flex items-center gap-x-6">
+            {/* full navbar */}
+            <Nav />
+            <ThemeToggler />
+
+            {/* mobile screen nav */}
+            <div className="xl:hidden">
+              <MobileNav />
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
